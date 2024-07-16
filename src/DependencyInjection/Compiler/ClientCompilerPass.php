8f2340c994ff501e364bb369b9d899391a5599f7
John Mortlock <jmortlock@lmg.com.au>
2024-07-16 15:20:17 +0930

    Address PR feedback

--- src/DependencyInjection/Compiler/ClientCompilerPass.php
+++ src/DependencyInjection/Compiler/ClientCompilerPass.php
@@ -55,7 +55,7 @@ final class ClientCompilerPass implements CompilerPass
             $serviceClient = definition(ServiceClient::class, [$client['address']])
                 ->setFactory([GrpcServiceClient::class, 'create']);
 
-            if (isset($client['clientKey'])) {
+            if ($client['clientKey'] ?? false && $client['clientPem'] ?? false) {
                 $serviceClient = definition(ServiceClient::class, [
                     $client['address'],
                     null, // root CA - Not required for Temporal Cloud
