e8a2a25cb379b84cfc4b5ab8cc3555fddf2088f2
Vlad Shashkov <shashkov.root@gmail.com>
2024-11-23 19:04:28 +0700

    [Config] Add grpcContext

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -10,9 +10,11 @@ declare(strict_types=1);
 
 namespace Vanta\Integration\Symfony\Temporal\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface as BundleConfiguration;
 
+use Temporal\Internal\Support\DateInterval;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 use Temporal\Api\Enums\V1\QueryRejectCondition;
@@ -25,6 +27,22 @@ use Temporal\WorkerFactory;
  *  roadrunnerRPC: non-empty-string,
  * }
  *
+ * @phpstan-type GrpcContext array{
+ *  timeout: array{
+ *    value: positive-int,
+ *    format: \DateInterval::FORMAT_*,
+ *  },
+ *  options: array<non-empty-string, scalar>,
+ *  metadata: array<non-empty-string, scalar>,
+ *  retryOptions: array{
+ *    initialInterval: ?non-empty-string,
+ *    maximumInterval: ?non-empty-string,
+ *    backoffCoefficient: float,
+ *    maximumAttempts: int<0, max>,
+ *    nonRetryableExceptions: array<class-string<\Throwable>>
+ *  },
+ * }
+ *
  * @phpstan-type Client array{
  *  name: non-empty-string,
  *  address: non-empty-string,
@@ -35,7 +53,7 @@ use Temporal\WorkerFactory;
  *  interceptors: list<non-empty-string>,
  *  clientKey: ?non-empty-string,
  *  clientPem: ?non-empty-string,
- * }
+ *  grpcContext: GrpcContext,
  *
  * @phpstan-type ScheduleClient array{
  *   name: non-empty-string,
@@ -127,109 +145,6 @@ final class Configuration implements BundleConfiguration
                     ->end()
                 ->end()
             ->end()
-
-            ->children()
-                ->arrayNode('clients')
-                ->defaultValue(['default' => [
-                    'namespace'     => 'default',
-                    'address'       => env('TEMPORAL_ADDRESS')->__toString(),
-                    'dataConverter' => 'temporal.data_converter'],
-                ])
-                ->useAttributeAsKey('name')
-                    ->arrayPrototype()
-                        ->addDefaultsIfNotSet()
-                        ->children()
-                            ->scalarNode('namespace')
-                                ->isRequired()->cannotBeEmpty()
-                            ->end()
-                            ->scalarNode('address')
-                                ->defaultValue(env('TEMPORAL_ADDRESS')->__toString())->cannotBeEmpty()
-                            ->end()
-                            ->scalarNode('identity')
-                            ->end()
-                            ->scalarNode('dataConverter')
-                                ->cannotBeEmpty()->defaultValue('temporal.data_converter')
-                            ->end()
-                            ->scalarNode('clientKey')
-                            ->end()
-                            ->scalarNode('clientPem')
-                            ->end()
-                            ->enumNode('queryRejectionCondition')
-                                ->values([
-                                    QueryRejectCondition::QUERY_REJECT_CONDITION_UNSPECIFIED,
-                                    QueryRejectCondition::QUERY_REJECT_CONDITION_NONE,
-                                    QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_OPEN,
-                                    QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_COMPLETED_CLEANLY,
-                                ])
-                                ->validate()
-                                    ->ifNotInArray([
-                                        QueryRejectCondition::QUERY_REJECT_CONDITION_UNSPECIFIED,
-                                        QueryRejectCondition::QUERY_REJECT_CONDITION_NONE,
-                                        QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_OPEN,
-                                        QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_COMPLETED_CLEANLY,
-                                    ])
-                                    ->thenInvalid(sprintf('"queryRejectionCondition" value is not in the enum: %s', QueryRejectCondition::class))
-                                ->end()
-                            ->end()
-                            ->arrayNode('interceptors')
-                                ->validate()
-                                    ->ifTrue(static fn (array $values): bool => !(count($values) == count(array_unique($values))))
-                                    ->thenInvalid('Should not be repeated interceptor')
-                                ->end()
-                                ->defaultValue([])
-                                ->scalarPrototype()->end()
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end()
-            ->end()
-
-
-
-            ->children()
-                ->arrayNode('scheduleClients')
-                    ->defaultValue(['default' => [
-                        'namespace'     => 'default',
-                        'address'       => env('TEMPORAL_ADDRESS')->__toString(),
-                        'dataConverter' => 'temporal.data_converter'],
-                    ])
-                    ->useAttributeAsKey('name')
-                        ->arrayPrototype()
-                            ->addDefaultsIfNotSet()
-                            ->children()
-                                ->scalarNode('namespace')
-                                    ->isRequired()->cannotBeEmpty()
-                                ->end()
-                                ->scalarNode('address')
-                                    ->defaultValue(env('TEMPORAL_ADDRESS')->__toString())->cannotBeEmpty()
-                                ->end()
-                                ->scalarNode('identity')
-                                ->end()
-                                ->scalarNode('dataConverter')
-                                    ->cannotBeEmpty()->defaultValue('temporal.data_converter')
-                                ->end()
-                                ->enumNode('queryRejectionCondition')
-                                    ->values([
-                                        QueryRejectCondition::QUERY_REJECT_CONDITION_UNSPECIFIED,
-                                        QueryRejectCondition::QUERY_REJECT_CONDITION_NONE,
-                                        QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_OPEN,
-                                        QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_COMPLETED_CLEANLY,
-                                    ])
-                                    ->validate()
-                                        ->ifNotInArray([
-                                            QueryRejectCondition::QUERY_REJECT_CONDITION_UNSPECIFIED,
-                                            QueryRejectCondition::QUERY_REJECT_CONDITION_NONE,
-                                            QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_OPEN,
-                                            QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_COMPLETED_CLEANLY,
-                                        ])
-                                        ->thenInvalid(sprintf('"queryRejectionCondition" value is not in the enum: %s', QueryRejectCondition::class))
-                                    ->end()
-                                ->end()
-                        ->end()
-                    ->end()
-                ->end()
-            ->end()
-
 
             ->children()
                 ->arrayNode('workers')
@@ -360,6 +275,209 @@ final class Configuration implements BundleConfiguration
             ->end()
         ;
 
+
+        $clients = $treeBuilder->getRootNode()
+            ->children()
+                ->arrayNode('clients')
+                ->defaultValue(['default' => [
+                    'namespace'     => 'default',
+                    'address'       => env('TEMPORAL_ADDRESS')->__toString(),
+                    'dataConverter' => 'temporal.data_converter'],
+                ])
+                ->useAttributeAsKey('name')
+        ;
+
+        $this->addClient($clients);
+
+        $scheduleClients = $treeBuilder->getRootNode()
+            ->children()
+            ->arrayNode('scheduleClients')
+            ->defaultValue(['default' => [
+                'namespace'     => 'default',
+                'address'       => env('TEMPORAL_ADDRESS')->__toString(),
+                'dataConverter' => 'temporal.data_converter'],
+            ])
+            ->useAttributeAsKey('name')
+        ;
+
+        $this->addClient($scheduleClients);
+
         return $treeBuilder;
+    }
+
+
+    private function addClient(ArrayNodeDefinition $node): void
+    {
+        $dateIntervalValidator = static function(?string $v): bool {
+            if ($v == null){
+                return false;
+            }
+
+            try {
+                $value = \DateInterval::createFromDateString($v);
+            }catch (\DateMalformedIntervalStringException){
+                return true;
+            }
+
+            if ($value === false){
+                return true;
+            }
+
+            return true;
+        };
+
+
+
+
+        //@formatter:off
+        $node->arrayPrototype()
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->scalarNode('namespace')
+                    ->isRequired()->cannotBeEmpty()
+                ->end()
+                ->scalarNode('address')
+                    ->defaultValue(env('TEMPORAL_ADDRESS')->__toString())->cannotBeEmpty()
+                ->end()
+                ->scalarNode('identity')
+                ->end()
+                ->scalarNode('dataConverter')
+                    ->cannotBeEmpty()->defaultValue('temporal.data_converter')
+                ->end()
+                ->scalarNode('clientKey')
+                ->end()
+                ->scalarNode('clientPem')
+                ->end()
+                ->enumNode('queryRejectionCondition')
+                    ->values([
+                        QueryRejectCondition::QUERY_REJECT_CONDITION_UNSPECIFIED,
+                        QueryRejectCondition::QUERY_REJECT_CONDITION_NONE,
+                        QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_OPEN,
+                        QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_COMPLETED_CLEANLY,
+                    ])
+                    ->validate()
+                        ->ifNotInArray([
+                            QueryRejectCondition::QUERY_REJECT_CONDITION_UNSPECIFIED,
+                            QueryRejectCondition::QUERY_REJECT_CONDITION_NONE,
+                            QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_OPEN,
+                            QueryRejectCondition::QUERY_REJECT_CONDITION_NOT_COMPLETED_CLEANLY,
+                        ])
+                        ->thenInvalid(sprintf('"queryRejectionCondition" value is not in the enum: %s', QueryRejectCondition::class))
+                    ->end()
+                ->end()
+                ->arrayNode('interceptors')
+                    ->validate()
+                        ->ifTrue(static fn (array $values): bool => !(count($values) == count(array_unique($values))))
+                        ->thenInvalid('Should not be repeated interceptor')
+                    ->end()
+                    ->defaultValue([])
+                    ->scalarPrototype()->end()
+                ->end()
+                ->arrayNode('grpcContext')
+                    ->children()
+                        ->arrayNode('timeout')
+                            ->children()
+                                ->integerNode('value')
+                                    ->info('Value connection timeout')
+                                ->end()
+                                ->enumNode('format')
+                                    ->info('Interval unit')
+                                    ->defaultValue(DateInterval::FORMAT_SECONDS)
+                                    ->values([
+                                        DateInterval::FORMAT_NANOSECONDS,
+                                        DateInterval::FORMAT_MICROSECONDS,
+                                        DateInterval::FORMAT_MILLISECONDS,
+                                        DateInterval::FORMAT_SECONDS,
+                                        DateInterval::FORMAT_MINUTES,
+                                        DateInterval::FORMAT_HOURS,
+                                        DateInterval::FORMAT_DAYS,
+                                        DateInterval::FORMAT_WEEKS,
+                                        DateInterval::FORMAT_MONTHS,
+                                        DateInterval::FORMAT_YEARS,
+                                    ])
+                                    ->validate()
+                                        ->ifNotInArray([
+                                            DateInterval::FORMAT_NANOSECONDS,
+                                            DateInterval::FORMAT_MICROSECONDS,
+                                            DateInterval::FORMAT_MILLISECONDS,
+                                            DateInterval::FORMAT_SECONDS,
+                                            DateInterval::FORMAT_MINUTES,
+                                            DateInterval::FORMAT_HOURS,
+                                            DateInterval::FORMAT_DAYS,
+                                            DateInterval::FORMAT_WEEKS,
+                                            DateInterval::FORMAT_MONTHS,
+                                            DateInterval::FORMAT_YEARS,
+                                        ])
+                                        ->thenInvalid(sprintf('"format" value is not in the enum: %s', DateInterval::class))
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('options')
+                            ->normalizeKeys(false)
+                            ->defaultValue([])
+                            ->prototype('variable')->end()
+                        ->end()
+                        ->arrayNode('metadata')
+                            ->normalizeKeys(false)
+                            ->defaultValue([])
+                            ->prototype('variable')->end()
+                        ->end()
+                            ->arrayNode('retryOptions')
+                                ->children()
+                                    ->scalarNode('initialInterval')
+                                        ->defaultNull()
+                                        ->info('Backoff interval for the first retry. Example: 30 seconds')
+                                        ->validate()
+                                             ->ifTrue($dateIntervalValidator)
+                                             ->thenInvalid('{client,scheduleClients}.initialInterval is not valid')
+                                        ->end()
+                                    ->end()
+                                    ->scalarNode('maximumInterval')
+                                    ->defaultNull()
+                                        ->info(
+                                            <<<STRING
+                                                Maximum backoff interval between retries. 
+                                                Exponential backoff leads to interval increase. 
+                                                This value is the cap of the interval.
+                                                Example: 30 seconds
+                                            STRING
+                                        )
+                                    ->end()
+                                    ->floatNode('backoff_coefficient')
+                                        ->info(
+                                            <<<STRING
+                                                Coefficient used to calculate the next retry backoff interval. 
+                                                The next retry interval is previous interval multiplied by this coefficient. 
+                                                Note: Must be greater than 1.0
+                                            STRING
+                                        )
+                                    ->end()
+                                    ->integerNode('maximumAttempts')
+                                        ->info(
+                                            <<<STRING
+                                                Maximum number of attempts. 
+                                                When exceeded the retries stop even if not expired yet. 
+                                                If not set or set to 0, it means unlimited
+                                            STRING
+                                        )
+                                    ->end()
+                                    ->arrayNode('nonRetryableExceptions')
+                                        ->scalarPrototype()
+                                            ->info(
+                                                <<<STRING
+                                                    Non-Retriable errors. This is optional. 
+                                                    Temporal server will stop retry if error type matches this list.
+                                                STRING
+                                            )
+                                        ->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ->end();
     }
 }
