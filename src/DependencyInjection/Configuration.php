d7258ac2098e394a265e4d02d395728de2de80df
Dmitry Kaufman <d.kaufman@pos-credit.ru>
2026-04-15 15:08:02 +0700

    ref #SPT-1212 fix interceptors config

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,6 +17,7 @@ use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface as BundleConfiguration;
 use Symfony\Component\DependencyInjection\Loader\Configurator\EnvConfigurator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
 use Temporal\Api\Enums\V1\QueryRejectCondition;
 use Temporal\Internal\Support\DateInterval;
 use Temporal\Worker\WorkerFactoryInterface;
@@ -430,6 +431,12 @@ final class Configuration implements BundleConfiguration
                     ->end()
                 ->end()
                 ->arrayNode('interceptors')
+                    ->arrayPrototype()
+                        ->children()
+                            ->stringNode('service_id')->end()
+                        ->end()
+                    ->end()
+                    ->info('Interceptors service ids')
                     ->validate()
                         ->ifTrue(static fn (array $values): bool => !(count($values) == count(array_unique($values))))
                         ->thenInvalid('Should not be repeated interceptor')
